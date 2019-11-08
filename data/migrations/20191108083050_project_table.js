4fdd4e101868b7198e4849d9996c16465f7631a0
admin <admin@admins-MacBook-Pro.local>
2019-11-08 08:44:46 -0800

    migrations table completed

---
+++ data/migrations/20191108083050_project_table.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex) {
+  return knex.schema
+     .createTable('resource', tbl => {
+          tbl.increments()
+          tbl.string('name',255).notNullable()
+          tbl.string('description',255)
+     })
+     .createTable('project', tbl => {
+          tbl.increments()
+          tbl.string('name',255).notNullable()
+          tbl.string('description',255)
+          tbl.boolean()
+     })
+     .createTable('resource_project', tbl => {
+          tbl.increments()
+          tbl.integer('resource_id')
+               .unsigned()
+               .notNullable()
+               .references('id')
+               .inTable('resource')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE')
+          tbl.integer('project_id')
+               .unsigned()
+               .notNullable()
+               .references('id')
+               .inTable('project')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE')
+     })
+     .createTable('tasks', tbl => {
+          tbl.string('description', 255).notNullable()
+          tbl.string('notes', 255)
+          tbl.boolean()
+          tbl.integer('project_id')
+               .unsigned()
+               .notNullable()
+               .references('id')
+               .inTable('project')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE')
+     })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+     .dropTableIfExists('tasks')
+     .dropTableIfExists('resource_project')
+     .dropTableIfExists('project')
+     .dropTableIfExists('resource')
+};
